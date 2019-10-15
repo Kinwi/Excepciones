f0b59d6b293e794adae3b78a7086fe2f0adcd5c5
Raúl López Hernando <raullopezhernando@gmail.com>
2019-10-15 10:04:54 +0200

    Ultimos cambios

---
+++ Ejemplo/Ejemplo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int resultado = 23 / Convert.ToInt32(Console.ReadLine());
+            }
+            catch (DivideByZeroException)
+            {
+
+                Console.WriteLine("No se puede dividir entre 0");
+            }
+
+            finally
+            {
+                // Siempre va a saltar el finally aunque no haya un error
+
+                Console.WriteLine("Vuleve a intentarlo");
+            }
+        }
+    }
+}
---
+++ Ejemplo/Ejemplo1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int resultado = 23 / Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
